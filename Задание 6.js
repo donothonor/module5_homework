d0c85494ae2218ecf222a07337a37f2dbc03477b
VADIM <nagiozv2@gmail.com>
2021-07-01 17:34:28 +0400

    fixed

--- Задание 6.js
+++ Задание 6.js
@@ -1,8 +1,12 @@
-let arr = [3, 3, 1]
+let arr = [3, 3, 3]
 let isEqual = true
-let reference = arr
+let reference = arr[0]
+
+
 arr.forEach(function(item) {
-    
+    if (item != reference) {
+        isEqual = false
+    }
 })
 
 console.log(isEqual)
